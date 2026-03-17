48301f7b9ca04e0e58af5904eb6f705c9b224e33
msayracode <msayracode@gmail.com>
2026-03-17 10:48:20 +0300

    jQuery added and chart linked to real poll values

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
 from django.template import loader
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.http import JsonResponse
@@ -58,5 +58,8 @@ def resultData(request, obj):
   print(votedata)
   return JsonResponse(votedata, safe=False)
       
+
+
+
 
 
